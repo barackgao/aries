8467fd56b567b79fc6b1bd0e57d77b16e8392150
barackgao <G.Wei.China@gmail.com>
2016-12-09 11:46:09 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/aries.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aries.dir/src/main.cpp.o"
+  "aries.pdb"
+  "aries"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aries.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
